1552be954d94c2371e191560acea6ebad2c6ecbb
Ani-Torosyan <anitorosyan080804@gmail.com>
2025-03-06 17:13:45 +0400

    minor changes

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -14,7 +14,7 @@ function ErrorMessage() {
   const error = searchParams.get("error");
 
   return error === "not-a-teacher" ? (
-    <div style={{ color: "red", fontWeight: "bold" }}>
+    <div style={{ color: "red", fontWeight: "bold", textAlign: "center" }}>
       You are not authorized to review student assignments.
     </div>
   ) : null;
@@ -24,19 +24,21 @@ export default function Home() {
   return (
     <ClerkProvider>
       <div className="bg-custom h-screen w-full">
+        <div className="h-[10vh]"></div>
+        <Suspense fallback={null}>
+          <ErrorMessage />
+        </Suspense>
+
         <div className="flex items-center justify-center mt-10">
           <div className="flex flex-col items-center justify-center max-w-[988px] gap-y-5 text-center mt-10">
-            <Suspense fallback={<Loader className="h-5 w-5 text-muted-foreground animate-spin" />}>
-              <ErrorMessage />
-            </Suspense>
-
-            <div className="h-[10vh]"></div>
             <h1 className="text-xl lg:text-3xl font-bold text-customDark max-w-[480px] mt-10">
               Teach, review, and grade with ArmLing!
             </h1>
+            
             <ClerkLoading>
               <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
             </ClerkLoading>
+
             <ClerkLoaded>
               <SignedOut>
                 <SignInButton forceRedirectUrl={"/reviews/writing-review"}>
@@ -46,9 +48,6 @@ export default function Home() {
                 </SignInButton>
               </SignedOut>
               <SignedIn>
-                <Suspense fallback={null}>
-                  <ErrorMessage />
-                </Suspense>
                 <Button size="lg" variant="secondary" className="w-full" asChild>
                   <Link href="/reviews/writing-review">I am a teacher</Link>
                 </Button>
@@ -56,14 +55,19 @@ export default function Home() {
             </ClerkLoaded>
           </div>
         </div>
+
+        <div className="h-[5vh]"></div>
+
         <div className="flex items-center justify-center">
           <img src="/teachers-cropped.svg" className="h-1/2 w-1/2 mt-10" alt="Hero" />
         </div>
       </div>
+
       <Footer />
     </ClerkProvider>
   );
 }
+
 
 
 
