01f9ba9d67ae48e00ef55e94c7798b29c9e28940
Maria Vardanyan <88451540+mey17@users.noreply.github.com>
2025-04-17 00:52:03 +0400

    Update heart

--- app/api/heart-refill/route.ts
+++ app/api/heart-refill/route.ts
@@ -1,42 +1,37 @@
 import { NextResponse } from 'next/server';
-import { heartRefillJob } from '@/jobs/heartRefillJob';
-import User from '@/modals/user.modal';
 import { connect } from '@/db';
-
-export async function GET() {
-  try {
-    await heartRefillJob();
-    return new NextResponse("Heart refill job executed successfully.", { status: 200 });
-  } catch (error) {
-    console.error("Error executing heart refill job:", error);
-    return new NextResponse("Failed to execute heart refill job.", { status: 500 });
-  }
-}
+import User from '@/modals/user.modal';
 
 export async function PUT(request: Request) {
   try {
     await connect();
 
     const body = await request.json();
-    const { userId, newHearts, newLastUpdate } = body;
+    const { userId } = body;
 
-    if (!userId || newHearts == null || !newLastUpdate) {
-      return NextResponse.json({ message: "User ID, new hearts, and new last update are required" }, { status: 400 });
+    if (!userId) {
+      return NextResponse.json({ message: 'User ID is required' }, { status: 400 });
     }
 
     const user = await User.findOne({ clerkId: userId });
 
     if (!user) {
-      return NextResponse.json({ message: "User not found" }, { status: 404 });
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
 
+    // Decrement hearts and update lastHeartUpdate
+    const newHearts = Math.max(0, user.userHearts - 1);
     user.userHearts = newHearts;
-    user.lastHeartUpdate = new Date(newLastUpdate);
+    user.lastHeartUpdate = new Date(); // Update the lastHeartUpdate field
     await user.save();
 
-    return NextResponse.json({ message: "Hearts updated successfully", hearts: newHearts });
+    return NextResponse.json({
+      message: 'Hearts updated successfully',
+      hearts: newHearts,
+      lastHeartUpdate: user.lastHeartUpdate,
+    });
   } catch (error) {
-    console.error("Error updating user hearts:", error);
-    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    console.error('Error updating user hearts:', error);
+    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
   }
 }
