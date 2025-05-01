02b69126cd373c5626bf612006f3dffbca2a4a5c
SheroyanMagdalena <sheroyanmagdalena@gmail.com>
2025-05-02 02:35:06 +0400

    recommendations books ui update

--- app/(main)/recommendations/books/page.tsx
+++ app/(main)/recommendations/books/page.tsx
@@ -9,7 +9,7 @@ const allGenres = [
   "Գիտական ֆանտաստիկա", "Արկածային", "Ֆանտաստիկա", "Մանկական գրականություն"
 ];
 
-const levels = ["A1", "A2", "B1", "B2", "C1", "C2"];
+const levels = ["A1", "A2", "B1", "B2", "C1"];
 
 const FilterPopover = ({
   genres,
